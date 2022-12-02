9dcaaba7e928ac901d6a3f522389619f19dfc884
Benjamin Larsson <f.benjamin.larsson@gmail.com>
2022-12-02 17:23:34 +0100

    Complete day 02 part 1

---
+++ day02/day02b.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from functools import *
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+print("DAY 02a")
+
+shape_to_score = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+result_matrix = {
+    ("A", "X"): 3,
+    ("B", "Y"): 3,
+    ("C", "Z"): 3,
+
+    ("A", "Y"): 6,
+    ("B", "Z"): 6,
+    ("C", "X"): 6,
+
+    ("A", "Z"): 0,
+    ("B", "X"): 0,
+    ("C", "Y"): 0,
+}
+
+draw_score = 3
+win_score = 6
+
+with open("input.txt", "r") as file:
+    games = [tuple(line.strip().split(" ")) for line in file]
+
+    score = 0
+    for (opponent_play, my_play) in games:
+        score += shape_to_score[my_play]
+        score += result_matrix[(opponent_play, my_play)]
+
+    print(f"If I follow the strategy guide fully, and its predictions are entirely nice and accurate, my total score will be {score}")
