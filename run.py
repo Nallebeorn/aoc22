ef45c4e208a781c4933e022566d273726b7576ab
Benjamin <mafortion.benjamin@gmail.com>
2022-12-07 21:16:56 +0100

    Time run.py

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 import os
 import sys
+import time
 from runpy import run_path
 
 day_folders_contents = [[os.path.join(root, file) for file in files] for root, dirs, files in os.walk(os.path.dirname(sys.argv[0])) if "day" in root]
 scripts = sorted([script for folder in day_folders_contents for script in folder if script.endswith(".py")]) # flatten
 
+start_time = time.perf_counter()
 for script in scripts:
     run_path(script)
     print("")
     if (script.endswith("b.py")):
-        print("")
+        print("")        
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+
+print(f"Running all scripts took {elapsed_time * 1000}ms")
