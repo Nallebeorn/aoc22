0e3d83b686f2a52428d3cde58920442e13459c16
Benjamin <mafortion.benjamin@gmail.com>
2022-12-09 21:46:05 +0100

    Complete day 8 part 2

--- day08/day08a.py
+++ day08/day08a.py
@@ -16,10 +16,10 @@ with open("input.txt", "r") as file:
     for y, row in enumerate(grid):
         for x, height in enumerate(row):
             total_trees_visible += any([
-                all(grid[y][xx] < height for xx in range(0, x)),
-                all(grid[y][xx] < height for xx in range(x + 1, len(row))),
-                all(grid[yy][x] < height for yy in range(0, y)),
-                all(grid[yy][x] < height for yy in range(y + 1, len(grid))),
+                all([grid[y][xx] < height for xx in range(0, x)]),
+                all([grid[y][xx] < height for xx in range(x + 1, len(row))]),
+                all([grid[yy][x] < height for yy in range(0, y)]),
+                all([grid[yy][x] < height for yy in range(y + 1, len(grid))]),
             ])
             
     print(f"There are {total_trees_visible} trees visible from outside the grid.")
--- day08/day08b.py
+++ day08/day08b.py
@@ -11,15 +11,32 @@ print(
 with open("input.txt", "r") as file:
     grid = [[char for char in line.strip()] for line in file]
 
-    total_trees_visible = 0
+    best_scenic_score_so_far = 0
     
     for y, row in enumerate(grid):
         for x, height in enumerate(row):
-            total_trees_visible += any([
-                all(grid[y][xx] < height for xx in range(0, x)),
-                all(grid[y][xx] < height for xx in range(x + 1, len(row))),
-                all(grid[yy][x] < height for yy in range(0, y)),
-                all(grid[yy][x] < height for yy in range(y + 1, len(grid))),
-            ])
+            scores = [0, 0, 0, 0]
             
-    print(f"There are {total_trees_visible} trees visible from outside the grid.")
+            for xx in reversed(range(0, x)):
+                scores[0] += 1
+                if grid[y][xx] >= height:
+                    break
+                
+            for xx in range(x + 1, len(row)):
+                scores[1] += 1
+                if grid[y][xx] >= height:
+                    break
+                
+            for yy in reversed(range(0, y)):
+                scores[2] += 1
+                if grid[yy][x] >= height:
+                    break
+                
+            for yy in range(y + 1, len(grid)):
+                scores[3] += 1
+                if grid[yy][x] >= height:
+                    break
+                
+            best_scenic_score_so_far = max(best_scenic_score_so_far, reduce(lambda a, b: a * b, scores, 1))
+            
+    print(f"It seems like {best_scenic_score_so_far} is the highest scenic score for any tree.")
