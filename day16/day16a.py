04016b4d9f98144a4b315fe88b1f72da51831f5f
Benjamin <mafortion.benjamin@gmail.com>
2022-12-17 22:08:30 +0100

    I think I'll need a different approach

--- day16/day16a.py
+++ day16/day16a.py
@@ -13,9 +13,9 @@ print(
 start_time = perf_counter()
     
 class State:
-    def __init__(self, path, pressure_release, time_remaining, opened_valves):
-        self.location = path[-1]
-        self.path = path
+    def __init__(self, location, path_since_open, pressure_release, time_remaining, opened_valves):
+        self.location = location
+        self.path_since_open = path_since_open
         self.pressure_release = pressure_release
         self.time_remaining = time_remaining
         self.opened_valves = opened_valves
@@ -35,11 +35,13 @@ with open("input.txt", "r") as file:
         valves[name] = (flow_rate, tunnels_to)
     
     queue: "deque[State]" = deque()
-    queue.append(State(["AA"], 0, 30, ["AA"])) # AA's valve hasn't been opened but it's flow rate is 0 so we can consider it opened
+    queue.append(State("AA", [], 0, 30, ["AA"])) # AA's valve hasn't been opened but it's flow rate is 0 so we can consider it opened
     
     current_best: State = None
     
     current_highest_pressure = 0
+    
+    num_valves_to_open = len([flow_rate for flow_rate, tunnels in valves.values() if flow_rate > 0]) + 1
     
     while len(queue):
         state = queue.popleft()
@@ -50,25 +52,25 @@ with open("input.txt", "r") as file:
         best_possible_remaining = state.pressure_release
         for valve in valves:
             if valve not in state.opened_valves:
-                best_possible_remaining += valves[valve][0] * state.time_remaining
+                best_possible_remaining += valves[valve][0] * (state.time_remaining - 1)
         if best_possible_remaining < current_highest_pressure:
             continue
         
         print(state, "::", best_possible_remaining, "/", current_highest_pressure, "::", len(state.opened_valves), "/", len(valves))
         
-        if state.time_remaining > 0 and len(state.opened_valves) < len(valves):
-            if state.location not in state.opened_valves:
-                pressure_released = flow_rate * state.time_remaining
-                queue.append(State(state.path + [state.location], state.pressure_release + pressure_released, state.time_remaining - 1, state.opened_valves + [state.location]))
+        if state.time_remaining > 0 and len(state.opened_valves) < num_valves_to_open:
+            if flow_rate > 0 and state.location not in state.opened_valves:
+                pressure_released = flow_rate * (state.time_remaining - 1)
+                queue.append(State(state.location, [], state.pressure_release + pressure_released, state.time_remaining - 1, state.opened_valves + [state.location]))
             
             for to in tunnels:
-                if to != state.opened_valves[-1]:
-                    queue.append(State(state.path + [to], state.pressure_release, state.time_remaining - 1, state.opened_valves))
+                if to not in state.path_since_open:
+                    queue.append(State(to, state.path_since_open + [to], state.pressure_release, state.time_remaining - 1, state.opened_valves))
         else:
             if current_best == None or state.pressure_release > current_best.pressure_release:
                 current_best = state
     
-    print("BEST: ", current_best, current_best.path)
+    print(f"If I do this methodically I should be able to release {current_best.pressure_release} units of pressure and save the elephants and me before the volcano erupts!")
 
 end_time = perf_counter()
 print(f"[took {(end_time - start_time) * 1000}ms]")
